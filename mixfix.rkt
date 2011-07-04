43d816b55ba451d2697767e2badfbecccc0b8a27
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2011-07-03 22:37:40 -0400

    Use topological sort; link in tonyg/ometa.plt

--- mixfix.rkt
+++ mixfix.rkt
@@ -3,6 +3,16 @@
 (require srfi/1)
 (require racket/pretty)
 
+(require "graph.rkt")
+
+(require (planet tonyg/ometa))
+(require (only-in planet/resolver resolve-planet-path))
+(require (only-in racket/path path-only))
+(ometa-library-path (path-only (resolve-planet-path (list "ometa-boot.g" 'tonyg/ometa))))
+(displayln (ometa-library-path))
+(define-namespace-anchor mixfix)
+(ometa-namespace-getter (lambda () (namespace-anchor->namespace mixfix)))
+
 ;; An Associativity is one of
 ;;  - 'left
 ;;  - 'right
@@ -13,7 +23,9 @@
 
 ;; A Piece is one of
 ;;  - '_
-;;  - a Symbol (other than '_)
+;;  - a Symbol (other than '_), referring to a terminal
+;;  - a String, referring to a terminal
+;;  - a (list Symbol), referring to a nonterminal
 
 ;; A GroupName is a Symbol
 
@@ -30,7 +42,10 @@
 (struct precedence (high low) #:transparent)
 
 ;; A Grammar is a (grammar Symbol ListOf<Grouping> ListOf<Precedence>)
-(struct grammar (start-symbol groupings precedences) #:transparent)
+(struct grammar (start-symbol groupings grouping-names precedence-graph)
+	#:omit-define-syntaxes
+	#:transparent
+	#:constructor-name grammar*)
 
 (define-syntax Left (syntax-rules () ((_ piece ...) (operator 'left (list 'piece ...)))))
 (define-syntax Right (syntax-rules () ((_ piece ...) (operator 'right (list 'piece ...)))))
@@ -69,12 +84,14 @@
       (set! p (drop-right p 1)))
     p))
 
+(define (variant** name group-name)
+  (string->symbol (string-append (symbol->string group-name) "::" name)))
+
 (define (grouping->productions g group-name)
   (let ((grouping (find (lambda (gr) (eq? group-name (grouping-name gr)))
 			(grammar-groupings g)))
-	(tighter-groups (map precedence-high
-			     (filter (lambda (pr) (eq? group-name (precedence-low pr)))
-				     (grammar-precedences g))))
+	(tighter-groups (let ((upstream (graph-edges-from (grammar-precedence-graph g) group-name)))
+			  (filter (lambda (n) (memq n upstream)) (grammar-grouping-names g))))
 	(start-symbol (grammar-start-symbol g)))
     (define (make-or alts)
       (cond
@@ -89,13 +106,17 @@
     (define (op-clause pred)
       (map (lambda (o)
 	     (make-seq (map (lambda (piece)
-			      (if (eq? piece '_)
-				  start-symbol
-				  `(t ,piece)))
+			      (cond
+			       ((eq? piece '_) `(apply ,start-symbol))
+			       ((and (list? piece) (null? (cdr piece))) `(apply ,(car piece)))
+			       (else `(exactly ,piece))))
 			    (operator-pieces/trim o))))
 	   (filter pred (grouping-operators grouping))))
-    (let ((up-alts (map (lambda (other-group)
-			  `(hat ,other-group))
+    (define (variant* name [group-name group-name])
+      (variant** name group-name))
+    (define (variant name [group-name group-name])
+      `(apply ,(variant* name group-name)))
+    (let ((up-alts (map (lambda (other-group) (variant "hat" other-group))
 			tighter-groups))
 	  (closed-alts (op-clause operator-closed?))
 	  (non-alts (op-clause operator-non?))
@@ -117,46 +138,58 @@
 				(v? left-alts))))
 	    (want-non (and (v? up-alts)
 			   (v? non-alts))))
-	`((def (hat ,group-name) ,(make-or (append (care (make-seq (list `(op-closed ,group-name)))
-							 (v? closed-alts))
-						   (care (make-seq (list `(up ,group-name)
-									 `(op-non ,group-name)
-									 `(up ,group-name)))
-							 want-non)
-						   (care (make-seq (list `(-> ,group-name)
-									 `(up ,group-name)))
-							 want-right)
-						   (care (make-seq (list `(up ,group-name)
-									 `(<- ,group-name)))
-							 want-left))))
-	  ,@(care `(def (-> ,group-name) ,(make-or (append (care `(op-prefix ,group-name)
-								 (v? prefix-alts))
-							   (care (make-seq
-								  (list `(up ,group-name)
-									`(op-right ,group-name)))
-								 (v? right-alts)))))
+	`((,(variant* "hat") ,(make-or (append (care (make-seq (list (variant "op-closed")))
+						     (v? closed-alts))
+					       (care (make-seq (list (variant "up")
+								     (variant "op-non")
+								     (variant "up")))
+						     want-non)
+					       (care (make-seq (list `(many1 ,(variant "right"))
+								     (variant "up")))
+						     want-right)
+					       (care (make-seq (list (variant "up")
+								     `(many1 ,(variant "left"))))
+						     want-left))))
+	  ,@(care `(,(variant* "right") ,(make-or (append (care (variant "op-pre")
+								(v? prefix-alts))
+							  (care (make-seq
+								 (list (variant "up")
+								       (variant "op-right")))
+								(v? right-alts)))))
 		  want-right)
-	  ,@(care `(def (<- ,group-name) ,(make-or (append (care `(op-postfix ,group-name)
-								 (v? postfix-alts))
-							   (care (make-seq 
-								  (list `(op-left ,group-name)
-									`(up ,group-name)))
-								 (v? left-alts)))))
+	  ,@(care `(,(variant* "left") ,(make-or (append (care (variant "op-post")
+							       (v? postfix-alts))
+							 (care (make-seq 
+								(list (variant "op-left")
+								      (variant "up")))
+							       (v? left-alts)))))
 		  want-left)
-	  ,@(care `(def (up ,group-name) ,(make-or up-alts))
-		  (v? up-alts))
-	  ,@(care `(def (op-closed ,group-name) ,(make-or (op-clause operator-closed?)))
-		  (v? closed-alts))
-	  ,@(care `(def (op-non ,group-name) ,(make-or (op-clause operator-non?)))
-		  want-non)
-	  ,@(care `(def (op-prefix ,group-name) ,(make-or (op-clause operator-prefix?)))
-		  (v? prefix-alts))
-	  ,@(care `(def (op-right ,group-name) ,(make-or (op-clause operator-right?)))
-		  (v? right-alts))
-	  ,@(care `(def (op-postfix ,group-name) ,(make-or (op-clause operator-postfix?)))
-		  (v? postfix-alts))
-	  ,@(care `(def (op-left ,group-name) ,(make-or (op-clause operator-left?)))
-		  (v? left-alts)))))))
+	  ,@(care `(,(variant* "up") ,(make-or up-alts)) (v? up-alts))
+	  ,@(care `(,(variant* "op-closed") ,(make-or closed-alts)) (v? closed-alts))
+	  ,@(care `(,(variant* "op-non") ,(make-or non-alts)) want-non)
+	  ,@(care `(,(variant* "op-pre") ,(make-or prefix-alts)) (v? prefix-alts))
+	  ,@(care `(,(variant* "op-right") ,(make-or right-alts)) (v? right-alts))
+	  ,@(care `(,(variant* "op-post") ,(make-or postfix-alts)) (v? postfix-alts))
+	  ,@(care `(,(variant* "op-left") ,(make-or left-alts)) (v? left-alts)))))))
+
+(define (grammar->ometa g)
+  `((,(grammar-start-symbol g) (or ,@(map (lambda (grouping)
+					    `(apply ,(variant** "hat"
+								(grouping-name grouping))))
+					  (grammar-groupings g))))
+    ,@(append-map (lambda (grouping) (grouping->productions g (grouping-name grouping)))
+		  (grammar-groupings g))))
+
+(define (grammar start-symbol groupings precedences)
+  (let* ((graph (make-graph (map (lambda (p) (list (precedence-low p)
+						   (precedence-high p)))
+				 precedences)))
+	 (sorted-grouping-names (graph-topological-sort graph)))
+    (grammar* start-symbol
+	      (map (lambda (n) (find (lambda (gr) (eq? n (grouping-name gr))) groupings))
+		   sorted-grouping-names)
+	      sorted-grouping-names
+	      graph)))
 
 ;;---------------------------------------------------------------------------
 
@@ -172,9 +205,9 @@
 		 (grouping 'not
 			   (Postfix !))
 		 (grouping 'top
-			   (Non b)
-			   (Non n)
-			   (Non OPAREN _ CPAREN))
+			   (Non (variable))
+			   (Non (literal))
+			   (Non "(" _ ")"))
 		 (grouping 'if
 			   (Non if _ then _ else _)))
 	   (list (precedence 'top 'if)
@@ -186,6 +219,21 @@
 		 (precedence 'add 'eq)
 		 (precedence 'eq 'and))))
 
-(for-each (lambda (grouping)
-	    (pretty-print (grouping->productions G (grouping-name grouping))))
-	  (grammar-groupings G))
+(pretty-print (grammar->ometa G))
+
+(simple-ometa-driver `((variable (seq (bind n (anything))
+				      (action (if (symbol? n)
+						  n
+						  (error 'expected 'variable)))))
+		       (literal (seq (bind n (anything))
+				     (action (if (number? n)
+						 n
+						 (error 'expected 'literal)))))
+		       ,@(grammar->ometa G))
+		     'expr
+		     '(1 - 2 + 3 == 2 + 0)
+		     (lambda (sv tail err)
+		       (pretty-print `((sv ,sv)
+				       (tail ,(input-stream->list tail))
+				       (err ,(format-ometa-error err)))))
+		     report-ometa-error)
