19b3af22ec3eb260d3e4584e473d605337376389
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-11-16 11:48:05 -0500

    Add Apache Licence v2.0, per request

--- graph.rkt
+++ graph.rkt
@@ -1,6 +1,17 @@
 #lang racket/base
-
-(require racket/set)
+;; Copyright 2011 Tony Garnock-Jones <tonyg@leastfixedpoint.com>
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;;     http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
 
 (provide graph?
 	 make-graph
@@ -9,6 +20,8 @@
 	 graph-add-edge!
 
 	 graph-topological-sort)
+
+(require racket/set)
 
 (struct graph (nodes forward-edges backward-edges))
 
